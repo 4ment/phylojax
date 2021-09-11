a543e211f5c46598a52b3b636adaa92f48bc2401
Mathieu Fourment <m.fourment@gmail.com>
2021-09-12 09:53:44 +1000

    Vectorize JC69 and formatting

--- phylojax/substitution.py
+++ phylojax/substitution.py
@@ -1,6 +1,7 @@
+from abc import ABC, abstractmethod
+
 import jax.numpy as np
 import jax.numpy.linalg as LA
-from abc import ABC, abstractmethod
 
 
 class SubstitutionModel(ABC):
@@ -23,35 +24,50 @@ class SubstitutionModel(ABC):
 
 class JC69(SubstitutionModel):
     def __init__(self):
-        SubstitutionModel.__init__(self, np.array([0.25]*4))
+        super().__init__(np.array([0.25] * 4))
 
     def p_t(self, d):
+        d = np.expand_dims(d, axis=-1)
         a = 0.25 + 3 / 4 * np.exp(-4 / 3 * d)
         b = 0.25 - 0.25 * np.exp(-4 / 3 * d)
-        return np.concatenate([a, b, b, b,
-                               b, a, b, b,
-                               b, b, a, b,
-                               b, b, b, a]).reshape((d.shape[0], 4, 4))
+        return np.concatenate(
+            [a, b, b, b, b, a, b, b, b, b, a, b, b, b, b, a], axis=-1
+        ).reshape(d.shape[:-1] + (4, 4))
 
     def q(self):
-        return np.array([[-1., 1./3, 1./3, 1./3],
-                              [1./3, -1., 1./3, 1./3],
-                              [1./3, 1./3, -1., 1./3],
-                              [1./3, 1./3, 1./3, -1.]])
+        return np.array(
+            [
+                [-1.0, 1.0 / 3, 1.0 / 3, 1.0 / 3],
+                [1.0 / 3, -1.0, 1.0 / 3, 1.0 / 3],
+                [1.0 / 3, 1.0 / 3, -1.0, 1.0 / 3],
+                [1.0 / 3, 1.0 / 3, 1.0 / 3, -1.0],
+            ]
+        )
 
-class SymmetricSubstitutionModel(SubstitutionModel):
+
+class SymmetricSubstitutionModel(SubstitutionModel, ABC):
     def __init__(self, frequencies):
-        SubstitutionModel.__init__(self, frequencies)
+        super().__init__(self, frequencies)
 
     def p_t(self, branch_lengths):
         Q = self.q()
         Q /= SubstitutionModel.norm(Q, self.frequencies)
         sqrt_pi = np.diag(np.sqrt(self.frequencies))
-        sqrt_pi_inv = np.diag(1. / np.sqrt(self.frequencies))
+        sqrt_pi_inv = np.diag(1.0 / np.sqrt(self.frequencies))
         S = sqrt_pi @ Q @ sqrt_pi_inv
         e, v = self.eigen(S)
-        return sqrt_pi_inv @ v @ (np.expand_dims(np.exp(e * np.expand_dims(branch_lengths, axis=1)), axis=1) * np.eye(
-            e.shape[0])) @ LA.inv(v) @ sqrt_pi
+        return (
+            sqrt_pi_inv
+            @ v
+            @ (
+                np.expand_dims(
+                    np.exp(e * np.expand_dims(branch_lengths, axis=1)), axis=1
+                )
+                * np.eye(e.shape[0])
+            )
+            @ LA.inv(v)
+            @ sqrt_pi
+        )
 
     def eigen(self, Q):
         return LA.eigh(Q)
@@ -59,29 +75,74 @@ class SymmetricSubstitutionModel(SubstitutionModel):
 
 class GTR(SymmetricSubstitutionModel):
     def __init__(self, rates, frequencies):
-        SymmetricSubstitutionModel.__init__(self, frequencies)
+        super().__init__(frequencies)
         self.rates = rates
 
     def q(self):
         rates = self.rates
-        # if len(rates) == 5:
-        #     rates.append(torch.tensor(np.array([1.])))
         pi = self.frequencies
-        return np.hstack((-(rates[0]*pi[1] + rates[1]*pi[2] + rates[2]*pi[3]), rates[0]*pi[1], rates[1]*pi[2], rates[2]*pi[3],
-                               rates[0]*pi[0], -(rates[0]*pi[0] + rates[3]*pi[2] + rates[4]*pi[3]), rates[3]*pi[2], rates[4]*pi[3],
-                               rates[1]*pi[0], rates[3]*pi[1], -(rates[1]*pi[0] + rates[3]*pi[1] + rates[5]*pi[3]), rates[5]*pi[3],
-                               rates[2]*pi[0], rates[4]*pi[1], rates[5]*pi[2], -(rates[2]*pi[0] + rates[4]*pi[1] + rates[5]*pi[2]))).reshape((4, 4))
+        return np.hstack(
+            (
+                -(
+                    rates[..., 0] * pi[..., 1]
+                    + rates[..., 1] * pi[..., 2]
+                    + rates[..., 2] * pi[..., 3]
+                ),
+                rates[..., 0] * pi[..., 1],
+                rates[..., 1] * pi[..., 2],
+                rates[..., 2] * pi[..., 3],
+                rates[..., 0] * pi[..., 0],
+                -(
+                    rates[..., 0] * pi[..., 0]
+                    + rates[..., 3] * pi[..., 2]
+                    + rates[..., 4] * pi[..., 3]
+                ),
+                rates[..., 3] * pi[..., 2],
+                rates[..., 4] * pi[..., 3],
+                rates[..., 1] * pi[..., 0],
+                rates[..., 3] * pi[..., 1],
+                -(
+                    rates[..., 1] * pi[..., 0]
+                    + rates[..., 3] * pi[..., 1]
+                    + rates[..., 5] * pi[..., 3]
+                ),
+                rates[..., 5] * pi[..., 3],
+                rates[..., 2] * pi[..., 0],
+                rates[..., 4] * pi[..., 1],
+                rates[..., 5] * pi[..., 2],
+                -(
+                    rates[..., 2] * pi[..., 0]
+                    + rates[..., 4] * pi[..., 1]
+                    + rates[..., 5] * pi[..., 2]
+                ),
+            )
+        ).reshape(self.rates.shape[:-1] + (4, 4))
 
 
 class HKY(SymmetricSubstitutionModel):
     def __init__(self, kappa, frequencies):
-        SymmetricSubstitutionModel.__init__(self, frequencies)
+        super().__init__(frequencies)
         self.kappa = kappa
 
     def q(self):
         pi = self.frequencies
-        return np.hstack((-(pi[1] + self.kappa*pi[2] + pi[3]), pi[1], self.kappa*pi[2], pi[3],
-                          pi[0], -(pi[0] + pi[2] + self.kappa*pi[3]), pi[2], self.kappa*pi[3],
-                          self.kappa*pi[0], pi[1], -(self.kappa*pi[0] + pi[1] + pi[3]), pi[3],
-                          pi[0], self.kappa*pi[1], pi[2], -(pi[0] + self.kappa*pi[1] + pi[2]))).reshape((4, 4))
-
+        return np.hstack(
+            (
+                -(pi[..., 1] + self.kappa * pi[..., 2] + pi[..., 3]),
+                pi[..., 1],
+                self.kappa * pi[..., 2],
+                pi[..., 3],
+                pi[..., 0],
+                -(pi[..., 0] + pi[..., 2] + self.kappa * pi[..., 3]),
+                pi[..., 2],
+                self.kappa * pi[..., 3],
+                self.kappa * pi[..., 0],
+                pi[..., 1],
+                -(self.kappa * pi[..., 0] + pi[..., 1] + pi[..., 3]),
+                pi[..., 3],
+                pi[..., 0],
+                self.kappa * pi[..., 1],
+                pi[..., 2],
+                -(pi[..., 0] + self.kappa * pi[..., 1] + pi[..., 2]),
+            )
+        ).reshape(self.kappa.shape[:-1] + (4, 4))
--- test/test_substitution.py
+++ test/test_substitution.py
@@ -1,19 +1,21 @@
 import jax
-import pytest
 import jax.numpy as np
-
-from phylojax.substitution import JC69, GTR
+import pytest
 from jax.config import config
+
+from phylojax.substitution import GTR, JC69
+
 config.update("jax_enable_x64", True)
+
 
 @pytest.mark.parametrize("t", [0.001, 0.1])
 def test_JC69(t):
-    ii = 1. / 4. + 3. / 4. * np.exp(- 4. / 3. * t)
-    ij = 1./4. - 1./4. * np.exp(- 4./3. * t)
+    ii = 1.0 / 4.0 + 3.0 / 4.0 * np.exp(-4.0 / 3.0 * t)
+    ij = 1.0 / 4.0 - 1.0 / 4.0 * np.exp(-4.0 / 3.0 * t)
     subst_model = JC69()
     P = subst_model.p_t(np.expand_dims(np.array([t]), axis=-1))
-    assert ii == pytest.approx(P[0, 0, 0].item(), 1.0e-6)
-    assert ij == pytest.approx(P[0, 0, 1].item(), 1.0e-6)
+    assert ii == pytest.approx(P[0, 0, 0, 0].item(), 1.0e-6)
+    assert ij == pytest.approx(P[0, 0, 0, 1].item(), 1.0e-6)
 
 
 def test_GTR():
@@ -22,11 +24,15 @@ def test_GTR():
     subst_model = GTR(rates, freqs)
     P = subst_model.p_t(np.array([[0.1]]))
     P_expected = np.array(
-        [[[
-            [0.93717830, 0.009506685, 0.047505899, 0.005809115],
-            [0.02640748, 0.894078744, 0.006448058, 0.073065722],
-            [0.16158572, 0.007895626, 0.820605951, 0.009912704],
-            [0.01344433, 0.060875872, 0.006744752, 0.918935042],
-        ]]]
+        [
+            [
+                [
+                    [0.93717830, 0.009506685, 0.047505899, 0.005809115],
+                    [0.02640748, 0.894078744, 0.006448058, 0.073065722],
+                    [0.16158572, 0.007895626, 0.820605951, 0.009912704],
+                    [0.01344433, 0.060875872, 0.006744752, 0.918935042],
+                ]
+            ]
+        ]
     )
     assert jax.numpy.allclose(P, P_expected, rtol=1e-05)
