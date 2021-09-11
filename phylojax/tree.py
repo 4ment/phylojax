3b5c39da6ae15399b4b82555bb843d255f53907a
Mathieu Fourment <m.fourment@gmail.com>
2021-09-12 09:59:47 +1000

    Vectorize height transform

--- phylojax/tree.py
+++ phylojax/tree.py
@@ -1,6 +1,6 @@
 import jax.numpy as np
-import jax
-from jax import jit
+import jax.ops
+
 
 def distance_to_ratios(tree):
     taxa_count = len(tree.taxon_namespace)
@@ -14,38 +14,49 @@ def distance_to_ratios(tree):
             heights[node.index] = node.date
         else:
             bounds[node.index] = max([bounds[x.index] for x in node.child_node_iter()])
-            heights[node.index] = max([heights[c.index] + c.edge_length for c in node.child_node_iter()])
+            heights[node.index] = max(
+                [heights[c.index] + c.edge_length for c in node.child_node_iter()]
+            )
 
     for node in tree.preorder_node_iter():
         if node != tree.seed_node:
             if not node.is_leaf():
-                ratios[node.index - taxa_count] = (heights[node.index] - bounds[node.index])/(heights[node.parent_node.index] - bounds[node.index])
+                ratios[node.index - taxa_count] = (
+                    heights[node.index] - bounds[node.index]
+                ) / (heights[node.parent_node.index] - bounds[node.index])
 
     return ratios, heights[tree.seed_node.index], bounds
 
 
-
-# @jax.partial(jit, static_argnums=(1,2))
-# @jit
 def heights_to_branch_lengths(node_heights, bounds, indexing):
-    taxa_count = int((bounds.shape[0] + 1)/2)
+    taxa_count = int((bounds.shape[0] + 1) / 2)
     indices_sorted = indexing[np.argsort(indexing[:, 1])].transpose()
-    return np.concatenate((node_heights[indices_sorted[0, :taxa_count] - taxa_count] - bounds[:taxa_count],
-                      node_heights[indices_sorted[0, taxa_count:] - taxa_count] - node_heights[indices_sorted[1, taxa_count:] - taxa_count]))
+    return np.concatenate(
+        (
+            node_heights[..., indices_sorted[0, :taxa_count] - taxa_count]
+            - bounds[:taxa_count],
+            node_heights[..., indices_sorted[0, taxa_count:] - taxa_count]
+            - node_heights[..., indices_sorted[1, taxa_count:] - taxa_count],
+        ),
+        -1,
+    )
 
 
 def transform_ratios(root_height, ratios, bounds, indexing):
-    ratios2 = np.split(ratios, ratios.shape[0])
-    taxa_count = ratios.shape[0] + 2
-    heights = [None]*(taxa_count-1)
-    heights[-1] = root_height
-
-    for parent_id, id in indexing:
-        if id >= taxa_count:
-            heights[id - taxa_count] = bounds[id] + ratios2[id - taxa_count] * (
-                    heights[parent_id - taxa_count] - bounds[id])
-
-    return np.concatenate(heights)
+    taxa_count = ratios.shape[-1] + 2
+    x = np.concatenate((ratios, root_height), axis=-1)
+    heights = np.empty_like(x)
+    heights = jax.ops.index_update(heights, jax.ops.index[..., -1], x[..., -1])
+    for parent_id, id_ in indexing:
+        if id_ >= taxa_count:
+            heights = jax.ops.index_update(
+                heights,
+                jax.ops.index[..., id_ - taxa_count],
+                bounds[id_]
+                + x[..., id_ - taxa_count]
+                * (heights[..., parent_id - taxa_count] - bounds[id_]),
+            )
+    return heights
 
 
 def setup_indexes(tree):
@@ -69,7 +80,7 @@ def setup_dates(tree, heterochronous=False):
     if heterochronous:
         dates = {}
         for node in tree.leaf_node_iter():
-            dates[str(node.taxon)] = float(str(node.taxon).split('_')[-1][:-1])
+            dates[str(node.taxon)] = float(str(node.taxon).split("_")[-1][:-1])
 
         max_date = max(dates.values())
         min_date = min(dates.values())
@@ -90,4 +101,3 @@ def setup_dates(tree, heterochronous=False):
         oldest = None
 
     return oldest
-
