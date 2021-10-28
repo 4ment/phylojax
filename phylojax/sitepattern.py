0c892d33537c2f1ece9cab43ab4e0eb14e060156
Mathieu Fourment <m.fourment@gmail.com>
2021-10-28 15:08:34 +1100

    Add toml file

--- phylojax/sitepattern.py
+++ phylojax/sitepattern.py
@@ -1,6 +1,7 @@
+from typing import Any, List, Union
+
 import jax.numpy as np
-from typing import List, Union, Any
-import jax.ops
+
 
 def get_dna_leaves_partials_compressed(alignment):
     weights = []
@@ -22,17 +23,17 @@ def get_dna_leaves_partials_compressed(alignment):
             weights.append(patterns[indexes[i]])
 
     partials = []  # type: List[Union[List[None], Any]]
-    # partials = np.empty((len(alignment)*2-1, 4, len(weights)))
-    dna_map = {'a': [1.0, 0.0, 0.0, 0.0],
-               'c': [0.0, 1.0, 0.0, 0.0],
-               'g': [0.0, 0.0, 1.0, 0.0],
-               't': [0.0, 0.0, 0.0, 1.0]}
+    dna_map = {
+        'a': [1.0, 0.0, 0.0, 0.0],
+        'c': [0.0, 1.0, 0.0, 0.0],
+        'g': [0.0, 0.0, 1.0, 0.0],
+        't': [0.0, 0.0, 0.0, 1.0],
+    }
     for idx, name in enumerate(alignment):
         temp = []
         for i, c in enumerate(alignment[name].symbols_as_string()):
             if keep[i]:
-                temp.append(dna_map.get(c.lower(), [1., 1., 1., 1.]))
-        # partials = jax.ops.index_update(partials, jax.ops.index[idx, :, :], np.transpose(np.array(temp)))
+                temp.append(dna_map.get(c.lower(), [1.0, 1.0, 1.0, 1.0]))
         partials.append(np.transpose(np.array(temp)))
 
     for i in range(len(alignment) - 1):
